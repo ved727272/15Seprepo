5216e3ed71a3ebc3bb4d157d4bb81d523fc4bfc1
ved727272 <brijesh.zte@gmail.com>
2025-09-15 22:50:22 +0530

    Update main.tf

--- environment/main.tf
+++ environment/main.tf
@@ -21,6 +21,17 @@ module "subnet" {
 #     sql_admin_password = "Raman@12345"
 # }
 
+module "sql_database" {
+    depends_on = [ module.sql_server ]
+    source = "../modules/azurerm_sql_database"
+    resource_group_name = module.rg.resource_group_name
+    sql_server_name = module.sql_server.sql_server_name
+    sql_database_name = "raman_sqldb"
+    sku_name = "S0"
+  
+
+}
+
 # module "lb" {
 #     source = "../modules/azurerm_lb"
 #     resource_group_name = module.rg.resource_group_name
