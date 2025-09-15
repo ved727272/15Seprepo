580b50a7839b1f5ac21b3062e375ba5aae6fe34e
ved727272 <brijesh.zte@gmail.com>
2025-09-15 22:39:30 +0530

    First commit

--- modules/azurerm_subnet/main.tf
+++ modules/azurerm_subnet/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_subnet" "subnet" {
   name                 = "raman_subnet"
   resource_group_name  = "raman_rg"
   virtual_network_name = "raman_vnet"
-  address_prefixes     = ["10.0.0.1/24"]
+  address_prefixes     = ["10.0.0.0/24"]
   
 }
